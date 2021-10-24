62334449db479298f162672a74ed31fbf76c7eb5
Rustem Chanyshev <chanyshev92@gmail.com>
2021-10-24 16:47:02 +0500

    add Homework 5 folder

---
+++ Homework05/Homework05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Homework05 {
+    public static void main(String[] args) {
+		int a,test;
+		int min=9;
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("New number: ");
+		a=scanner.nextInt();
+		while (a!=-1){
+			while (a!=0){
+				test=a%10;
+				if (test<min) min=test;
+				a=a/10;
+			}
+			System.out.print("New number: ");
+			a=scanner.nextInt();
+			
+		}
+		
+        System.out.println("Mindigit: "+min);
+		
+    }
+	
+}
