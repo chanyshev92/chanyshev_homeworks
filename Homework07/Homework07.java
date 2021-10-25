9d5154034a4c90067169284f9be133cd275ce195
Rustem Chanyshev <chanyshev92@gmail.com>
2021-10-25 22:40:29 +0500

    add Homework07

---
+++ Homework07/Homework07.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Homework07 {
+    public static void main(String[] args) {
+		int a=-101;
+		int minCount=0,Number=-200;
+		int[] countArray=new int[201];
+		Scanner scanner=new Scanner(System.in);
+			
+		while (a!=-1){			
+			System.out.print("New Number: ");
+			a=scanner.nextInt();
+			countArray[a+100]++;
+			minCount=countArray[a+100];
+		}
+		
+		for(int i=0;i<201;i++){
+		if((minCount>=countArray[i])&&(countArray[i]>0)){ 
+			minCount=countArray[i]; 
+			Number=i-100;
+			}
+		}
+		
+        System.out.println("The Biggest Number with MinCount "+Number);
+    }
+}
