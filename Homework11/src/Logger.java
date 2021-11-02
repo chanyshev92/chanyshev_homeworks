84b2c358fb72dcecda0e0f9de7b348d380a13702
Rustem Chanyshev <chanyshev92@gmail.com>
2021-11-02 14:57:14 +0500

    new Homework11

--- Homework11/src/Logger.java
+++ Homework11/src/Logger.java
@@ -1,11 +1,5 @@
-import java.util.Objects;
 
 public class Logger {
-    private final String[] messages;
-
-    private static final int MAX_MESSAGES = 100;
-
-    private int count;
 
     private static final Logger instance;
     static{
@@ -14,34 +8,17 @@ public class Logger {
 
     private Logger(){
 
-        this.messages=new String[MAX_MESSAGES];
     }
     public static Logger getInstance(){
+
         return instance;
     }
 
-    public void add(String message) {
-        if (count < MAX_MESSAGES) {
-            this.messages[count] = message;
-            count++;
-        } else {
-            System.err.println("Переполнение журнала");
-        }
-    }
+
 
     public void log (String message){
-        if (count != 0) { int value=0;
-            for (int i=0;i<=count;i++){
-                if (Objects.equals(messages[i], message)){
-                    value++;
-                }
-            }
-            if(value == 0){
-                System.out.println("Запись не обнаружена");
-            }else {
-                System.out.println("Запись обнаружена "+value+" раз(а)");
-            }
-        }else System.out.println("Журнал пустой");
+        if (message != "") { System.out.println(message);
+        }else System.out.println("Сообщение пустое");
 
     }
 
--- Homework11/src/Main.java
+++ Homework11/src/Main.java
@@ -5,13 +5,8 @@ public class Main {
         Logger logger1 = Logger.getInstance();
         Logger logger2 = Logger.getInstance();
 
-        logger2.add("Все хорошо");
-        logger1.add("Ошибка1");
-        logger2.add("Предупреждение2");
-        logger2.add("Приказ66");
-
         logger1.log("Все хорошо");
-        logger2.log("Error");
+        logger2.log("");
 
     }
 }
