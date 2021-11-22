5303a25e9994ac12131183632ed2bd0c32f13e97
Rustem Chanyshev <chanyshev92@gmail.com>
2021-11-22 15:01:05 +0500

    modify Main

--- Attestation01/src/Main.java
+++ Attestation01/src/Main.java
@@ -12,12 +12,14 @@ public class Main {
 
         User user1 = usersRepository.findByID(2);
 
-        user1.setAge(18);
-        user1.setName("Света");
+        user1.setAge(55);
+        user1.setName("Светлана");
         user1.setWorker(false);
 
         System.out.println("Update");
         usersRepository.update(user1);
+
+        users= usersRepository.findAll();
 
         for (User user : users) {
             user.print();
